ac156696d026a39be392c7f4494775a8e3ef9a2c
Crista Minns <crisminns@gmail.com>
2022-10-25 13:41:30 -0400

    new codewars kata

---
+++ squaring_an_argument.js
@@ -0,0 +1,8 @@
+//Now you have to write a function that takes an argument and returns the square of it.
+
+
+// My solution - Write the "square"-function here
+function square(arg) {
+    return arg * arg
+}
+square(2)
